11c6ba6091576d58b85430fe12a488e96efe3f0b
hrvojedondivic <hrvoje.dondivic@gmail.com>
2023-04-24 23:03:22 +0200

    add 'Callable' type hint to the return_to_main_menu_prompt method

--- main_menu_helpers.py
+++ main_menu_helpers.py
@@ -1,7 +1,8 @@
+from typing import Callable
 import constants
 
 
-def return_to_main_menu_prompt(foo, users: dict = None):
+def return_to_main_menu_prompt(foo: Callable[[dict], None], users: dict = None):
     user_input = input(constants.BACK_TO_MAIN_MENU_STRING)
 
     while user_input.lower().strip() != "da" and user_input.lower().strip() != "ne":
